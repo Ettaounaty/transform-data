4c7bc69f3f5e3801558c2b1a3a3d9c6e8c0ffd6f
Ettaounaty <106287027+Ettaounaty@users.noreply.github.com>
2024-01-02 17:47:44 +0100

    Update trans.py

--- trans.py
+++ trans.py
@@ -81,10 +81,11 @@ if uploaded_file is not None:
         data['Compte US'] = data['Compte US'].astype(str).str.replace('"', '')
         data['Compte Marocaine'] = data['Compte Marocaine'].astype(str).str.replace('=', '')
         data['Compte Marocaine'] = data['Compte Marocaine'].astype(str).str.replace('"', '')
+        data['montant'] = data['montant'].astype(str).str.replace(',', '')
 
         
         #Supprimer les lignes où la valeur dans la colonne “montant" égale à 0
-        data=data[(data['montant']!='0')]
+        data=data[(data['montant']!=0)]
         # Supprimer les lignes où 'compte marocaine' commence par '6' et 'analytic' est vide
         data = data[~((data['Compte Marocaine'].astype(str).str.startswith('6')) & (data['Analytic'].isnull()))]
         #remplacer la valeur de dpartement par "551" ou compte marocaine=71972001
